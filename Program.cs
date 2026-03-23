ad9602abc0bcfd47af8ae7b48c6cdffc00e81f17
Ioana Salaru <ioana.salaru@student.tuiasi.ro>
2026-03-23 15:02:47 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransportInfo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IModel model = new Model();
+            IView view = new ConsoleView(model);
+            IPresenter presenter = new Presenter(view, model);
+            view.SetPresenter(presenter);
+            ((ConsoleView)view).Start();
+        }
+    }
+}
