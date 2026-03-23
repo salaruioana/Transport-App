6b249e7befcb591d1f020f2d301515fda76e2fc0
Ioana Salaru <ioana.salaru@student.tuiasi.ro>
2026-03-23 22:44:57 +0200

    Implementare interfata grafica

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Windows.Forms;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
@@ -7,15 +8,28 @@ using System.Threading.Tasks;
 
 namespace TransportInfo
 {
+
     internal class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
             IModel model = new Model();
-            IView view = new ConsoleView(model);
+            //pentru varianta consola, se poate inlocui IView view new ConsoleView(model)
+            FormView view = new FormView(model);
             IPresenter presenter = new Presenter(view, model);
             view.SetPresenter(presenter);
-            ((ConsoleView)view).Start();
+
+            presenter.Init();
+            //asta se comenteaza pentru consola
+            view.RefreshCityLists();
+            //asta se decomenteaza pentru consola
+            //((ConsoleView)view).Start();
+            //asta se comenteaza pentru consola
+            Application.Run(view);
         }
     }
 }
